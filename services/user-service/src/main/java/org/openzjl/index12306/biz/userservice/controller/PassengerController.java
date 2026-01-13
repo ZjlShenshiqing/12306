75e1b1fa84fbc5ec762f2dc9bf3610fd5037e8fe
zjl <ssq_6@foxmail.com>
2026-01-13 17:42:07 +0800

    2026/1/13 commit-4 用户微服务 - 乘客控制层

---
+++ services/user-service/src/main/java/org/openzjl/index12306/biz/userservice/controller/PassengerController.java
@@ -0,0 +1,110 @@
+package org.openzjl.index12306.biz.userservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.openzjl.index12306.biz.userservice.dto.req.PassengerReqDTO;
+import org.openzjl.index12306.biz.userservice.dto.resp.PassengerActualRespDTO;
+import org.openzjl.index12306.biz.userservice.dto.resp.PassengerRespDTO;
+import org.openzjl.index12306.biz.userservice.service.PassengerService;
+import org.openzjl.index12306.framework.starter.convention.result.Result;
+import org.openzjl.index12306.framework.starter.idempotent.annotation.Idempotent;
+import org.openzjl.index12306.framework.starter.idempotent.enums.IdempotentSceneEnum;
+import org.openzjl.index12306.framework.starter.idempotent.enums.IdempotentTypeEnum;
+import org.openzjl.index12306.framework.starter.user.core.UserContext;
+import org.openzjl.index12306.framework.starter.web.Results;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 乘车人控制层
+ *
+ * @author zhangjlk
+ * @date 2026/1/13 17:07
+ */
+@RestController
+@RequiredArgsConstructor
+public class PassengerController {
+
+    private final PassengerService passengerService;
+
+    /**
+     * 根据用户名查询乘车人列表
+     *
+     * @return 乘车人列表
+     */
+    @GetMapping("/api/user-service/passenger/query")
+    public Result<List<PassengerRespDTO>> listPassengerQueryByUsername() {
+        return Results.success(passengerService.listPassengerQueryByUsername(UserContext.getUserName()));
+    }
+
+    /**
+     * 根据乘车人 ID 集合查询乘车人列表
+     *
+     * @param username 用户名
+     * @param ids      乘车人id集合
+     * @return         乘车人详细信息列表
+     */
+    @GetMapping("/api/user-service/inner/passenger/actual/query/ids")
+    public Result<List<PassengerActualRespDTO>> listPassengerQueryByIds(@RequestParam("username") String username, @RequestParam("ids") List<Long> ids) {
+        return Results.success(passengerService.listPassengerQueryByIds(username, ids));
+    }
+
+    /**
+     * 新增乘车人
+     *
+     * @param requestParam 新增乘车人请求参数
+     * @return 新增结果
+     */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(org.openzjl.index12306.framework.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在新增乘车人，请稍后再尝试..."
+    )
+    @PostMapping("/api/user-service/passenger/save")
+    public Result<Void> savePassenger(PassengerReqDTO requestParam) {
+        passengerService.savePassenger(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 修改乘车人
+     *
+     * @param requestParam 修改乘车人请求参数
+     * @return 修改结果
+     */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(org.openzjl.index12306.framework.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在修改乘车人，请稍后再尝试..."
+    )
+    @PostMapping("/api/user-service/passenger/update")
+    public Result<Void> updatePassenger(PassengerReqDTO requestParam) {
+        passengerService.updatePassenger(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 移除乘车人
+     *
+     * @param requestParam 移除乘车人请求参数
+     * @return 移除结果
+     */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(org.openzjl.index12306.framework.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在移除乘车人，请稍后再尝试..."
+    )
+    public Result<Void> removePassenger(PassengerReqDTO requestParam) {
+        passengerService.removePassenger(requestParam);
+        return Results.success();
+    }
+}
