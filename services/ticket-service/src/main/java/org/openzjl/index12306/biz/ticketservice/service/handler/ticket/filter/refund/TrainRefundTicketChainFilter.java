b5b9f0ebddc03d58c541623348d364b2ee407358
zjl <3169953986@qq.com>
2026-03-03 10:10:01 +0800

    2025/3/3 退款流程过滤器

---
+++ services/ticket-service/src/main/java/org/openzjl/index12306/biz/ticketservice/service/handler/ticket/filter/refund/TrainRefundTicketChainFilter.java
@@ -0,0 +1,19 @@
+package org.openzjl.index12306.biz.ticketservice.service.handler.ticket.filter.refund;
+
+import org.openzjl.index12306.biz.ticketservice.common.enums.TicketChainMarkEnum;
+import org.openzjl.index12306.biz.ticketservice.dto.req.RefundTicketReqDTO;
+import org.openzjl.index12306.framework.starter.designpattern.chain.AbstractChainHandler;
+
+/**
+ * 列车车票退款过滤器
+ *
+ * @author zhangjlk
+ * @date 2026/3/3 上午10:01
+ */
+public interface TrainRefundTicketChainFilter<T extends RefundTicketReqDTO> extends AbstractChainHandler<RefundTicketReqDTO> {
+
+        @Override
+        default String mark() {
+            return TicketChainMarkEnum.TRAIN_REFUND_TICKET_FILTER.name();
+        }
+}
