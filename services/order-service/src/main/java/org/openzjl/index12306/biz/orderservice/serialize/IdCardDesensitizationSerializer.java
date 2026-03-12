6714e04acbb66da56e9da68527de39a4086319b0
zjl <3169953986@qq.com>
2026-03-12 11:53:48 +0800

    2025/3/12  支付界面无法显示订单问题解决

--- services/order-service/src/main/java/org/openzjl/index12306/biz/orderservice/serialize/IdCardDesensitizationSerializer.java
+++ services/order-service/src/main/java/org/openzjl/index12306/biz/orderservice/serialize/IdCardDesensitizationSerializer.java
@@ -4,6 +4,7 @@
  */
 package org.openzjl.index12306.biz.orderservice.serialize;
 
+import cn.hutool.core.util.DesensitizedUtil;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
@@ -19,7 +20,12 @@ public class IdCardDesensitizationSerializer extends JsonSerializer<String> {
 
 
     @Override
-    public void serialize(String s, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
-
+    public void serialize(String idCard, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        if (idCard == null) {
+            jsonGenerator.writeNull();
+            return;
+        }
+        String idCardDesensitization = DesensitizedUtil.idCardNum(idCard, 4, 4);
+        jsonGenerator.writeString(idCardDesensitization);
     }
 }
